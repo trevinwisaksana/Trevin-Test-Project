930037d6900f66435d91c71c85f6c20df082e9f6
Trevin Wisaksana <trevinwisaksana@gmail.com>
2018-12-04 17:58:02 +0700

    Initial Commit

---
+++ Trevin-Test-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trevin-Test-Project
+//
+//  Created by Trevin Wisaksana on 04/12/18.
+//  Copyright © 2018 Trevin Wisaksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Trevin-Test-ProjectTests/Trevin_Test_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Trevin_Test_ProjectTests.swift
+//  Trevin-Test-ProjectTests
+//
+//  Created by Trevin Wisaksana on 04/12/18.
+//  Copyright © 2018 Trevin Wisaksana. All rights reserved.
+//
+
+import XCTest
+@testable import Trevin_Test_Project
+
+class Trevin_Test_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
